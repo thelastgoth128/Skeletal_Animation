2f05796b85974de031d2a72bc3743334d5ebbc27
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-27 17:18:01 +0200

    mesh class

--- src/mesh.h
+++ src/mesh.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <glm/glm.hpp>
 #include <iostream>
+#include <vector>
+#include "shaders/shader.h"
 
 using namespace std;
 
@@ -13,4 +15,20 @@ struct Vertex {
 struct Texture {
     unsigned int id;
     string type;
+};
+
+class Mesh {
+    public:
+        //mesh data
+        vector<Vertex>          vertices;
+        vector<unsigned int>    indices;
+        vector<Texture>         textures;
+
+        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
+        void Draw(Shader &shader);
+    private:
+        //render data
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh();
 };
