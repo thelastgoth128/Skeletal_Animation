e5aff383d799e1285a58c634442f161a54e5d006
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-28 00:43:05 +0200

    bone info structure

--- src/model.h
+++ src/model.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
@@ -10,8 +11,19 @@
 
 #include "shaders/shader.h"
 #include "mesh.h"
+#include "assimp_glm_helpers.h"
 
 using namespace std;
+
+struct BoneInfo
+{
+    /*id is index in finalBoneMatrices*/
+    int id;
+
+    /*offset matrix transforms vertex from model space to bone space*/
+    glm::mat4 offset;
+
+};
 
 
 class Model {
@@ -20,12 +32,20 @@ class Model {
             loadModel(path);
         }
         void Draw(Shader &shader);
-    public:
+    private:
+        map<string, BoneInfo> m_BoneInfoMap;
+        int m_BoneCounter = 0;
+
+        auto& GetBoneInfoMap() { return m_BoneInfoMap;}
+        int& GetBoneCount() { return m_BoneCounter; }
         //model data
         vector<Mesh> meshes;
         string directory;
         vector<Texture> textures_loaded;
 
+        void SetVertexBoneDataToDefault(Vertex& vertex);
+        void SetVertexBoneData(Vertex& vertex, int boneID, float weight);
+        void ExtractBoneWeightForVertices(std::vector<Vertex>& vertices, aiMesh* mesh, const aiScene* scene);
         void loadModel(string path);
         void processNode(aiNode *node, const aiScene *scene);
         Mesh processMesh(aiMesh *mesh, const aiScene *scene);
