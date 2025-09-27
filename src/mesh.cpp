ce64c8e2f921bf0914bcc0715a40229940b887d1
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-27 20:36:36 +0200

    draw function for the mesh

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -35,3 +35,29 @@ void Mesh::setupMesh() {
 
     glBindVertexArray(0);
 }
+
+void Mesh::Draw(Shader &shader) {
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
+    for(unsigned int i = 0; i < texture.size(); i++) {
+        glActiveTexture(GL_TEXTURE0 + i); //activate texture unit first
+        //retrieve texture number (the N in diffuse_textureN)
+        string number;
+        string name =textures[i].type;
+        if ( name == "texture_diffuse"){
+            number = to_string(diffuseNr++);
+        }
+        else if (name == "texture_specular"){
+            number = to_string(specularNr++);
+        }
+        
+        shader.setFloat(("material." + name + number).c_str(), i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+    glActiveTexture(GL_TEXTURE0);
+
+    //draw mesh
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
