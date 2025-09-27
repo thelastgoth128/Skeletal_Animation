098a428c4c1edcf9d5a374d36a8bc204b11971d5
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-27 17:11:33 +0200

    mesh vertex and texture attrib

---
+++ src/mesh.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <iostream>
+
+using namespace std;
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    string type;
+};
