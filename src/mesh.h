27dcb672dd60f28b4cebce760a9f63359b5fabaf
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-28 00:42:36 +0200

    add bone ids

--- src/mesh.h
+++ src/mesh.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include "shaders/shader.h"
+#define MAX_BONE_INFLUENCE 4
 
 using namespace std;
 
@@ -10,6 +11,16 @@ struct Vertex {
     glm::vec3 Position;
     glm::vec3 Normal;
     glm::vec2 TexCoords;
+
+    // tangent
+    glm::vec3 Tangent;
+    // bitangent
+    glm::vec3 Bitangent;
+
+    //bone indexes which will influence this vertex
+    int m_BoneIDs[MAX_BONE_INFLUENCE];
+    //weights from each bone
+    float m_Weights[MAX_BONE_INFLUENCE];
 };
 
 struct Texture {
