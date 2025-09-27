af8abecc06e29537efff05f715d2503ae1ee8733
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-28 00:42:23 +0200

    add vertex attrib for bone and weights

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -33,6 +33,14 @@ void Mesh::setupMesh() {
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
 
+    // ids
+    glEnableVertexAttribArray(3);
+    glVertexAttribIPointer(3, 4, GL_INT, sizeof(Vertex), (void*)offsetof(Vertex, m_BoneIDs));
+
+    // weights
+    glEnableVertexAttribArray(4);
+    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_Weights)); 
+
     glBindVertexArray(0);
 }
 
